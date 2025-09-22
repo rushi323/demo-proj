fa2394e6078f61b14d954f4c046e6130e3d80c07
Rushikesh Reddy <rushibreddy@gmail.com>
2025-09-22 16:21:58 -0400

    updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,10 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
+      version = "2.33.0"
     }
   }
 }
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-#test
